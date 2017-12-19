52591d5646216a29fe65fa7fef83f82316fde9ba
Sean Oughton <seanoughton@gmail.com>
2017-12-19 20:29:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function sayHiToGrandma(string) {
   else if (string === string.toLowerCase() ){
     return 'I can\'t hear you!' }
   else if (string === "I love you, Grandma.") {
-    return 'I love you, to.'
+    return 'I love you, too.'
   }
   
 }
